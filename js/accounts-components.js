0add64793eadc0c9caf53b0e0dbf0516f4b118b8
James <9418349+JamesFrancisLuke@users.noreply.github.com>
2018-01-29 16:40:16 +0000

    Working Auth
    Auth is now working...

---
+++ js/accounts-components.js
@@ -0,0 +1,25 @@
+app.accounts = {
+    /*For the user accounts only login setup etc*/
+    signUp: function (email, password) {
+        /*Creates an account, if no errors*/
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(function () {
+            app.components.removeOverlay('loading-cover');
+            Materialize.toast('Created an Account for: ' + email, 4000);
+        }).catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            Materialize.toast(errorMessage, 4000);
+        });
+    },
+    signIn: function (email, password) {
+        /*Signs user in, if correct */
+        firebase.auth().signInWithEmailAndPassword(email, password).then(function () {
+            app.components.removeOverlay('loading-cover');
+            Materialize.toast('Welcome back ' + email, 4000);
+        }).catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            Materialize.toast(errorMessage, 4000);
+        });
+    }
+}
