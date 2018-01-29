8b5519431946641feda9a6a48a4c79840890ab35
James <9418349+JamesFrancisLuke@users.noreply.github.com>
2018-01-29 17:33:17 +0000

    Onboarding Half-Implemented
    Needs to write to database, then some data tools need to be run on it.

--- js/accounts-components.js
+++ js/accounts-components.js
@@ -4,6 +4,7 @@ app.accounts = {
         /*Creates an account, if no errors*/
         firebase.auth().createUserWithEmailAndPassword(email, password).then(function () {
             app.components.removeOverlay('loading-cover');
+            app.components.showOnboarding(email);
             Materialize.toast('Created an Account for: ' + email, 4000);
         }).catch(function (error) {
             var errorCode = error.code;
@@ -21,5 +22,23 @@ app.accounts = {
             var errorMessage = error.message;
             Materialize.toast(errorMessage, 4000);
         });
+    },
+    logOut: function () {
+        /*Signs user out, if correct */
+        firebase.auth().signOut().then(function () {
+            window.location.reload(true);
+        }).catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            Materialize.toast(errorMessage, 4000);
+        });
+    },
+    data: {
+        /*For data manipulation*/
+        onboardComplete: function () {
+            /*For the Onboarding completion */
+            console.log("Ran...");
+            app.components.removeOverlay('loading-cover');
+        }
     }
 }
